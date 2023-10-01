85e1e912f78e98650f718e8690de5d53bc2a988f
Saam26 <saamyukkth@gmail.com>
2023-10-01 11:47:02 +0530

    proj2

---
+++ Drum_Kit_Project2/index.js
@@ -0,0 +1,40 @@
+
+// for (i=0;i<6;i++)
+// {
+//     document.querySelectorAll(".drum")[i].addEventListener("click",handleClick);
+// }
+var numberOfDrumButtons = document.querySelectorAll(".drum").length
+for (var i=0;i<numberOfDrumButtons;i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click",function()
+    {
+        var buttonInnerHTML = this.innerHTML;
+        switch(buttonInnerHTML)
+        {
+            case "w":
+                var audio = new Audio("/Users/samsidsuresh/Web development/Drum Kit Starting Files/sounds/crash.mp3");
+                audio.play();
+                break;
+            case "a":
+                var audio = new Audio("/Users/samsidsuresh/Web development/Drum Kit Starting Files/sounds/kick-bass.mp3");
+                audio.play();
+                break;
+            case "b":
+                var audio = new Audio("/Users/samsidsuresh/Web development/Drum Kit Starting Files/sounds/snare.mp3");
+                audio.play();
+                break;
+            case "c":
+                var audio = new Audio("/Users/samsidsuresh/Web development/Drum Kit Starting Files/sounds/tom-1.mp3");
+                audio.play();
+                break;
+            case "d":
+                var audio = new Audio("/Users/samsidsuresh/Web development/Drum Kit Starting Files/sounds/tom-2.mp3");
+                audio.play();
+                break;
+            case "e":
+                var audio = new Audio("/Users/samsidsuresh/Web development/Drum Kit Starting Files/sounds/tom-3.mp3");
+                audio.play();
+                break;
+        }
+    })
+}
